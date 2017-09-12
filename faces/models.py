72a65c7fab1f1c3abd82ca8f9286073791b6df60
shane.daly <shane.daly@wpengine.com>
2017-09-12 17:34:48 +0100

    delete faces

--- faces/models.py
+++ faces/models.py
@@ -1,4 +1,6 @@
+import os
 from django.db import models
+from django.dispatch import receiver
 
 from common.models import DateTimeMixin
 
@@ -8,3 +10,14 @@ class Face(DateTimeMixin, models.Model):
     face_image = models.ImageField()
     face_name = models.CharField(max_length=150, blank=True, null=True)
     unknown = models.BooleanField(default=False)
+
+
+@receiver(models.signals.post_delete, sender=Face)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Face` object is deleted.
+    """
+    if instance.face_image:
+        if os.path.isfile(instance.face_image.path):
+            os.remove(instance.face_image.path)
